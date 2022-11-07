7c962056415f19943c41567ad62f7a3f0609463a
Tito Mitto <mordeccai9@gmail.com>
2022-11-07 18:56:07 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<main class="h-screen flex items-center justify-center">
+  <div class="w-80 h-80 border-2 border-spacing-1 p-10 flex items-center justify-center  border-neutral-800 bg-white">
+    Proof of Payment
+  </div>
+</main>
+
