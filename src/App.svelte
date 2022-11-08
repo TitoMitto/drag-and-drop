f7ac3ba5d1197bfdf53e3911c3ca0ccf40748201
Tito Mitto <mordeccai9@gmail.com>
2022-11-08 07:24:11 +0300

    changed upload button color

--- src/App.svelte
+++ src/App.svelte
@@ -33,7 +33,7 @@
     on:dragover|preventDefault={onDragOverArea}
     on:dragleave={onLeaveDropArea}
     on:drop|preventDefault={onDropFile}
-    class:bg-slate-200={isDragging}
+    class:bg-slate-100={isDragging}
     class:border-slate-500={isDragging}
     class="w-80 h-80 border-2 border-dashed  rounded-md border-spacing-4 p-10 flex flex-col  items-center justify-center  border-neutral-200 bg-white"
   >
@@ -45,7 +45,7 @@
 
     <button
     class:bg-slate-50={isDragging}
-    class:border-slate-100={isDragging}
+    class:border-slate-200={isDragging}
       class="px-4 py-2 mt-5 rounded-md flex items-center border-solid border-2 font-medium text-blue-900"
     >
       <img class="h-5 mr-2" src="/upload.png" alt="File Icon" />
