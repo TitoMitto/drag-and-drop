268bd9144ad7ada0c6de51a4cc537607a623b33d
Tito Mitto <mordeccai9@gmail.com>
2022-11-08 06:16:56 +0300

    upload button

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,14 @@
 </script>
 <main class="h-screen  flex items-center bg-neutral-100 justify-center">
   <div class="w-80 h-80 border-2 border-dashed  rounded-md border-spacing-4 p-10 flex flex-col  items-center justify-center  border-neutral-200 bg-white">
-    <FileIcon/>
+    <img class="h-14" src="/file.png" alt="File Icon"/>
     <p class="font-semibold mt-2 text-blue-900">Proof of Payment</p>
     <p class="text-center text-neutral-500 mt-1">Upload or drag and drop <br> an image</p>
 
-    <button class="px-4 py-2 mt-5 rounded-md border-solid border-2 font-medium text-blue-900">Upload</button>
+    <button class="px-4 py-2 mt-5 rounded-md flex items-center border-solid border-2 font-medium text-blue-900">
+      <img class="h-5 mr-2" src="/upload.png" alt="File Icon"/>
+      Upload
+    </button>
 
   </div>
 </main>
